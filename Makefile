476757e1a256297a8f2249138e6cfd10a8d74b1f
tamaricoh <tamarcmail@gmail.com>
2024-07-11 16:25:51 +0300

    task 2

--- Makefile
+++ Makefile
@@ -1,22 +1,77 @@
-CC = gcc
+# # Compiler
+# CC = gcc 
 
-CFLAGS = -Wall -Wextra -m32
+# # Flags
+# CFlags = -Wall -Wextra -g
 
-TARGET = lab5
+# # The .c files (sources)
+# CFile = encoder.c
 
-SRCS = task1.c
+# # Sets the names of the .o files
+# OFile = $(CFile:.c=.o)
 
-OBJS = $(SRCS:.c=.o)
+# # Set the executable names
+# Ex = encoder
 
-all: $(TARGET)
+# # Declare as phony target, so the computer will differ them from files with the same name
+# .PHONY: all clean
 
-$(TARGET): $(OBJS)
-	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
+# all: $(Ex)
 
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
+# $(Ex): $(OFile)
+# 	$(CC) $(CFlags) -o $@ $^
 
-clean:
-	rm -f $(OBJS) $(TARGET)
+# %.o: %.c
+# 	$(CC) $(CFlags) -c -o $@ $<
 
-.PHONY: all clean
+# clean:
+# 	rm -f $(OFile) $(Ex)
+
+# setSpace :
+# 	chmod 700 -R ~
+
+# space : 
+# 	du -a | sort -n
+
+all: loader
+
+loader: task2.c
+	gcc -m32 -c task2.c -o task2.o
+	ld  -o loader task2.o startup.o start.o -L/usr/lib32 -lc -T linking_script -dynamic-linker /lib32/ld-linux.so.2
+
+# .PHONY: clean
+# clean:
+# 	rm -rf ./*.o loader
+
+
+# CC = gcc
+# LD = ld
+
+# CFLAGS = -Wall -Wextra -m32
+# LDFLAGS = -m elf_i386 -o lab5 task1.o startup.o start.o -T linking_script --dynamic-linker /lib/ld-linux.so.2 -lc
+
+# TARGET = lab5
+
+# SRCS = task2.c
+# OBJS = $(SRCS:.c=.o)
+
+# all: $(TARGET) loader
+
+# loader: lab5 start.o
+# 	gcc -m32 -c task2.c -o task2.o
+# 	ld  -o loader task2.o startup.o start.o -L/usr/lib32 -lc -T linking_script -dynamic-linker /lib32/ld-linux.so.2
+
+# $(TARGET): $(OBJS)
+# 	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
+
+# start.o: start.s
+# 	nasm -f elf -o start.o start.s
+
+# %.o: %.c
+# 	$(CC) $(CFLAGS) -c $< -o $@
+
+# clean:
+# 	rm -f $(OBJS) $(TARGET)
+# 	rm -rf ./*.o loader
+
+# .PHONY: all clean
---
+++ task2.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+
+#define BUF_SZ 4096
+
+void print_phdr_info(Elf32_Phdr *phdr) {
+    printf("Type: 0x%x\n", phdr->p_type);
+    printf("Offset: 0x%x\n", phdr->p_offset);
+    printf("Virtual Address: 0x%x\n", phdr->p_vaddr);
+    printf("Physical Address: 0x%x\n", phdr->p_paddr);
+    printf("File Size: 0x%x\n", phdr->p_filesz);
+    printf("Memory Size: 0x%x\n", phdr->p_memsz);
+    printf("Flags: 0x%x\n", phdr->p_flags);
+    printf("Alignment: 0x%x\n", phdr->p_align);
+    printf("\n");
+}
+
+void load_phdr(Elf32_Phdr *phdr, int fd, void *map_start) {
+    Elf32_Ehdr *header = (Elf32_Ehdr *)map_start;
+    
+    for (int i = 0; i < header->e_phnum; i++) {
+        Elf32_Phdr *current_phdr = &phdr[i];
+        
+        if (current_phdr->p_type == PT_LOAD) {
+            printf("Loading program header:\n");
+            print_phdr_info(current_phdr);
+            
+            int prot_flags = 0;
+            if (current_phdr->p_flags & PF_R)
+                prot_flags |= PROT_READ;
+            if (current_phdr->p_flags & PF_W)
+                prot_flags |= PROT_WRITE;
+            if (current_phdr->p_flags & PF_X)
+                prot_flags |= PROT_EXEC;
+            
+            void *addr = (void *)(uintptr_t)current_phdr->p_vaddr;
+            off_t offset = current_phdr->p_offset;
+            size_t filesz = current_phdr->p_filesz;
+            
+            // Map the segment into memory
+            void *mapped_addr = mmap(addr, filesz, prot_flags, MAP_PRIVATE | MAP_FIXED, fd, offset);
+            
+            if (mapped_addr == MAP_FAILED) {
+                perror("mmap failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <program>\n", argv[0]);
+        return 1;
+    }
+
+    // Open ELF file
+    int elf_fd = open(argv[1], O_RDONLY);
+    if (elf_fd == -1)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Map ELF file into memory
+    void *map_start = mmap(NULL, BUF_SZ, PROT_READ | PROT_WRITE, MAP_PRIVATE, elf_fd, 0);
+    if (map_start == MAP_FAILED)
+    {
+        perror("Error mapping file");
+        close(elf_fd);
+        return 1;
+    }
+
+    // Get ELF header and program header table
+    Elf32_Ehdr *header = (Elf32_Ehdr *)map_start;
+    Elf32_Phdr *phdr_table = (Elf32_Phdr *)((char *)map_start + header->e_phoff);
+
+    // Load program headers
+    load_phdr(phdr_table, elf_fd, map_start);
+
+    // Clean up: Unmap memory and close file
+    munmap(map_start, BUF_SZ);
+    close(elf_fd);
+
+    return 0;
+}
